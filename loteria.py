9aa300653f6c67f42394d0fa9566eeed0c2732b2
Enrique Delgado Rodríguez <yo@i22deroe.es>
2020-12-22 08:36:53 +0100

    Create loteria.py
    Initial release of loteria.py. I just coded this before I had my coffee, so can't be blamed for any bugs/undiserable behaviors. It won't (possibly) break your computer.
    Just call the script passing your numbers as arguments. It has no error control nor exception control whatsoever so try and write the numbers (5 figures) separated by spaces ( ). Anything else is likely to break it and make it exit.

---
+++ loteria.py
@@ -0,0 +1,36 @@
+import requests as r
+import time
+import sys
+
+from datetime import datetime as dt
+
+WAIT_TIME=30
+
+url= 'https://api.elpais.com/ws/LoteriaNavidadPremiados'
+
+def loteria(numeros):
+    res = dict(eval(r.get(url+'?n={}'.format(numeros[0])).text[9:]))
+    n_orig = len(numeros)
+    while res['status'] < 3:
+        print("[{}]: Comprobando números. {} han resultado premiados.".format(dt.now(), n_orig-len(numeros)))
+        for n in numeros:
+            res = dict(eval(r.get(url+'?n={}'.format(n)).text[9:]))
+            if res['premio']:
+                print("[{}]: ¡PREMIO AL NÚMERO {}: {}!".format(dt.now(), n, res['premio']))
+                numeros.remove(n)
+            time.sleep(WAIT_TIME)
+    else:
+        print("El sorteo ha terminado.")
+        return(0)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Por favor indica al menos un número.")
+        exit()
+    if len(sys.argv) > 1:
+        print("Vas a jugar con los números {}.".format(sys.argv[1:]))
+        loteria(sys.argv[1:])
+        exit()
+    else:
+        print("Por favor, indica al menos un número.")
+        exit()
